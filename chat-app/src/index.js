e5b2ff84460750a8f4eeb3afbb1aa7172d3a1f9c
Maryam Hashemi Asl <ms.hashemiasl@gmail.com>
2019-09-25 17:29:39 +0330

    Update index.js

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -9,14 +9,14 @@ require("firebase/firestore");
 
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDmPMLTjlx3h85ygoP9utNOuHyylPrtGG0",
-    authDomain: "chatapplication-319f3.firebaseapp.com",
-    databaseURL: "https://chatapplication-319f3.firebaseio.com",
-    projectId: "chatapplication-319f3",
-    storageBucket: "chatapplication-319f3.appspot.com",
-    messagingSenderId: "1013676921051",
-    appId: "1:1013676921051:web:8ce81ca432d9bb749cbcfe",
-    measurementId: "G-WPDDDD3244"
+  apiKey: "AIzaSyCKSV0vuqezFgXRl0DDIbUSMa_wrMd_lrk",
+  authDomain: "chatapplication-4aa76.firebaseapp.com",
+  databaseURL: "https://chatapplication-4aa76.firebaseio.com",
+  projectId: "chatapplication-4aa76",
+  storageBucket: "chatapplication-4aa76.appspot.com",
+  messagingSenderId: "487829345511",
+  appId: "1:487829345511:web:502abd82b6ab960ef33e42",
+  measurementId: "G-VXFGJT8TV3"
   });
 
 ReactDOM.render(<div>HELLO WORLD!!</div>, document.getElementById('root'));
